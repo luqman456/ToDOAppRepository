a75f5297e679e65d188a1a197052a93828d9b9ed
Luqman <muhammadluqmankhattak@outlook.com>
2022-03-19 16:11:53 +0500

    package create for fragment

--- app/src/main/java/com/multisync/todoapplication/fragment/add/AddFragment.kt
+++ app/src/main/java/com/multisync/todoapplication/fragment/add/AddFragment.kt
@@ -1,11 +1,11 @@
-package com.multisync.todoapplication
+package com.multisync.todoapplication.fragment.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.multisync.todoapplication.R
 
 
 class AddFragment : Fragment() {
--- app/src/main/java/com/multisync/todoapplication/fragment/list/ListFragment.kt
+++ app/src/main/java/com/multisync/todoapplication/fragment/list/ListFragment.kt
@@ -1,19 +1,20 @@
-package com.multisync.todoapplication
+package com.multisync.todoapplication.fragment.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.multisync.todoapplication.R
 
-class UpdateFragment : Fragment() {
+class ListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
 
