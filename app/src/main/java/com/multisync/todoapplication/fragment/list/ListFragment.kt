5e8817f3a0906ae1e3492ad0326cd9ca1edf802c
Luqman <muhammadluqmankhattak@outlook.com>
2022-04-09 00:17:12 +0500

    list adapter implementation done

--- app/src/main/java/com/multisync/todoapplication/fragment/list/ListFragment.kt
+++ app/src/main/java/com/multisync/todoapplication/fragment/list/ListFragment.kt
@@ -2,34 +2,51 @@ package com.multisync.todoapplication.fragment.list
 
 import android.os.Bundle
 import android.view.*
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.multisync.todoapplication.R
+import com.multisync.todoapplication.data.viewmodel.ToDoViewModel
+import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
+
+    private val mTodoViewModel: ToDoViewModel by viewModels()
+    private val adapter: ListAdapter by lazy { ListAdapter() }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val veiw = inflater.inflate(R.layout.fragment_list, container, false)
-        val floatingBtn = veiw?.findViewById<FloatingActionButton>(R.id.floatingActionButton)
-        floatingBtn?.setOnClickListener {
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val recyclerView = view.recyclerView
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        mTodoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            adapter.setData(data)
+        })
+
+        view.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
-        val listLayout = veiw?.findViewById<ConstraintLayout>(R.id.listLayout)
-        listLayout?.setOnClickListener {
+        view.listLayout?.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
         // Set Menu
         setHasOptionsMenu(true)
 
-        return veiw
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
